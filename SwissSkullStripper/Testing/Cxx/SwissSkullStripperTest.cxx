d034205ce72a5e7ee5c32ce81cdc87b999fc4c5a
Andras Lasso <lasso@queensu.ca>
2018-09-13 10:47:41 -0400

    Fixed automatic loading of default atlas
    When "Prefer executable CLI" mode was disabled, automatic loading of default atlas failed. The problem was that atlas files location was determined from executable path, which was Slicer.exe's location when CLI was loaded as DLL.
    
    Fixed by forcing running the module as separate executable. The performance impact is minimal, because startup time might increase by a few ten milliseconds, while computation takes tens of seconds.

--- SwissSkullStripper/Testing/Cxx/SwissSkullStripperTest.cxx
+++ SwissSkullStripper/Testing/Cxx/SwissSkullStripperTest.cxx
@@ -1,25 +1,11 @@
-#if defined(_MSC_VER)
-#pragma warning ( disable : 4786 )
-#endif
-
-#ifdef __BORLANDC__
-#define ITK_LEAN_AND_MEAN
-#endif
-
-#include "itkTestMain.h" 
-
-// STD includes
-#include <iostream>
-
-#ifdef WIN32
-# define MODULE_IMPORT __declspec(dllimport)
-#else
-# define MODULE_IMPORT
-#endif
-
-extern "C" MODULE_IMPORT int ModuleEntryPoint(int, char* []);
+#include "itkTestMain.h"
 
 void RegisterTests()
 {
-  StringToTestFunctionMap["ModuleEntryPoint"] = ModuleEntryPoint;
+  REGISTER_TEST(SwissSkullStripperTest);
 }
+
+#undef main
+#define main SwissSkullStripperTest
+
+#include "../../SwissSkullStripper.cxx" 
